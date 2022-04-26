dd2dd4bad4486f2ab0416233cd71a125e23161f8
Oscar Parada <oscar.parada@cinfo.es>
2022-04-26 16:15:19 +0200

    Especificamos el idioma de las peticiones

--- app/src/main/java/com/osovan/micartelera/model/TheMovieDbService.kt
+++ app/src/main/java/com/osovan/micartelera/model/TheMovieDbService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.Query
 
 interface TheMovieDbService {
 
-     @GET("movie/popular")
+     @GET("movie/popular?language=es")
      suspend fun getPopularMovies(
           @Query("api_key") apikey: String,
           @Query("region") region: String
